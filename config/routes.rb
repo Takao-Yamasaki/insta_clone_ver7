d2b951b4f5c51797416b57b41655deb300626c6d
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-05-24 17:36:47 +0900

    add User

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get 'samples/index'
   get '/health_check', to: 'health_checks#show'
   root to: "samples#index"
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 end
