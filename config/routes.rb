69f98c30cad6c14cb161a6a51c9ca096350596d3
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-05-28 10:54:15 +0900

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'samples/index'
   get '/health_check', to: 'health_checks#show'
-  root to: "samples#index"
+  root to: 'samples#index'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/login', to: 'user_sessions#new'
   post '/login', to: 'user_sessions#create'
-  delete '/logout', to: 'user_sessions#destory'
+  delete '/logout', to: 'user_sessions#destroy'
 end
