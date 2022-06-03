27c252ae68c663a56701cef0f23bf1e118406788
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-06-03 18:10:55 +0900

    add pagenation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :require_login, only: %i[new create edit update destroy]
   def index
-    @posts = Post.with_attached_images.includes(:user).order(created_at: :desc)
+    @pasy, @posts = pasy(Post.with_attached_images.includes(:user).order(created_at: :desc))
   end
 
   def new
