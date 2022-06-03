93cb5d318755c4fe0734de5c7f4834237453e013
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-06-03 09:47:44 +0900

    fix post_seed

--- db/seeds/posts.rb
+++ db/seeds/posts.rb
@@ -1,7 +1,8 @@
 puts 'Start inserting seed "posts"'
 User.limit(10).each do |user|
-  post = user.posts.create(body: Faker::Hacker.say_something_smart)
-  post.images.attach(io: File.open('db/fixtures/dummy.jpg'), filename: 'dummy')
-  post.images.attach(io: File.open('db/fixtures/dummy.jpg'), filename: 'dummy')
+  post = user.posts.build(body: Faker::Hacker.say_something_smart)
+  post.images.attach(io: File.open('db/fixtures/dummy.jpeg'), filename: 'dummy')
+  post.images.attach(io: File.open('db/fixtures/dummy.jpeg'), filename: 'dummy')
+  post.save
   puts "post#{post.id} has created!" 
 end
