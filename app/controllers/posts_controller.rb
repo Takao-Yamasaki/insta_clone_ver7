e330b0ed3ed9715178397e77f84f8d3213852777
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-06-03 18:46:22 +0900

    fix pagenation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :require_login, only: %i[new create edit update destroy]
   def index
-    @pasy, @posts = pasy(Post.with_attached_images.includes(:user).order(created_at: :desc))
+    @pagy, @posts = pagy(Post.with_attached_images.includes(:user).order(created_at: :desc))
   end
 
   def new
